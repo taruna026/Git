7dcfc9ae51e481b6f1bd6edde5bd295278094f3b
tarunagarg <taruna.garg@whitehatjr.com>
2021-06-12 10:26:15 +0530

    fix: dynamic port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 from service import getPersonInfo
 
 app = Flask(__name__)
@@ -12,4 +13,5 @@ def func():
     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 105)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port = port)
